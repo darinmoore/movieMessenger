d2237c3a487722c1cb156c7f063ce41045f05faa
Darin Moore <dsmoore@ucsd.edu>
2016-10-06 10:57:08 -0700

    Testing commands

--- app.py
+++ app.py
@@ -26,28 +26,38 @@ def webhook():
     # endpoint for processing incoming messaging events
 
     data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    log(data)  # log message for testint
 
     if data["object"] == "page":
 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
+                # if some one sent a message
+                if messaging_event.get("message"):  
+                    # the facebook ID of the person sending you the message
+                    sender_id = messaging_event["sender"]["id"]        
+                    # the recipient's ID, which should be your page's facebook ID
+                    recipient_id = messaging_event["recipient"]["id"]  
+                    # the message's text
+                    message_text = messaging_event["message"]["text"]  
 
-                if messaging_event.get("message"):  # someone sent us a message
+                    # if message isn't prefixed by '!' then help massage is shown
+                    if (message_text[0] != '!'):
+                        send_message(sender_id, message_text)
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    else:
+                        send_message(sender_id, "Working")
 
-                    send_message(sender_id, message_text)
-
-                if messaging_event.get("delivery"):  # delivery confirmation
+                # delivery confirmation
+                if messaging_event.get("delivery"):  
                     pass
 
-                if messaging_event.get("optin"):  # optin confirmation
+                # optin confirmation
+                if messaging_event.get("optin"):  
                     pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):  
                     pass
 
     return "ok", 200
