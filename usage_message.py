6e867bb0ec5611b0e60210cd0cf8129d9683c210
Darin Moore <dsmoore@ucsd.edu>
2016-11-12 13:00:47 -0800

    Hopefully importing works now

--- usage_message.py
+++ usage_message.py
@@ -1,5 +1,5 @@
-import send_message
+import send_message as send
 
 def usage_message(recipient_id):  # prints usage message for the user
-    send_message(recipient_id, "Welcome to Movie_Messenger, here is a list of some commands:\n\n" +
+    send.send_message(recipient_id, "Welcome to Movie_Messenger, here is a list of some commands:\n\n" +
         "'!movie <movie name>' will return information about the movie passed in")
